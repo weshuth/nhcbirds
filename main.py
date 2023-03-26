51003a373776f0e20c86bdf79ded786b6ec0ee04
weshuth <104175663+weshuth@users.noreply.github.com>
2023-03-26 11:09:35 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
-#pip install streamlit requests
-
 import requests
 import streamlit as st
 from PIL import Image
 from io import BytesIO
 import datetime
 
-#EBIRD_API_KEY = eBird_api_key
 API_BASE_URL = "https://api.ebird.org/v2"
-# IMAGE_API_URL = "<YOUR_IMAGE_API_URL>"
 
 st.title("Recent Bird Sightings in New Hanover County")
-
 
 def get_recent_sightings():
     today = datetime.date.today()
@@ -27,20 +22,32 @@ def get_recent_sightings():
 
 sightings = get_recent_sightings()
 
-for sighting in sightings:
-    bird_name = sighting["comName"]
-    #bird_img = get_bird_image(bird_name)
+# Pagination
+items_per_page = 10
+page_number = st.number_input(
+    label="Page Number", min_value=1, value=1, step=1, format="%i"
+)
+start_index = (page_number - 1) * items_per_page
+end_index = start_index + items_per_page
+sightings_to_display = sightings[start_index:end_index]
+
+for sighting in sightings_to_display:
+    bird_name = sighting.get("comName", "Unknown")
+    rarity_score = sighting.get("howMany", "Unknown")
+    species_code = sighting.get("speciesCode", "Unknown")
+    observation_date = sighting.get("obsDt", "Unknown")
 
     # Display bird info
     col1, col2 = st.columns(2)
     with col1:
-       # st.image(bird_img, width=200)
-        with col2:
-            st.header(bird_name)
-            st.subheader(f'Rarity Score: {sighting["howMany"]}')
-            st.write(f'Species Code: {sighting["speciesCode"]}')
-            st.write(f'Observation Date: {sighting["obsDt"]}')
-            # Add a small blurb about the bird
-            st.write("Bird description or blurb goes here.")
-            # Trending status could be calculated based on historical data or other factors
-            st.write("Trending: Upward/Downward")
+        # st.image(bird_img, width=200)  # Uncomment when you have an image API
+        pass
+    with col2:
+        st.header(bird_name)
+        st.subheader(f'Rarity Score: {rarity_score}')
+        st.write(f'Species Code: {species_code}')
+        st.write(f'Observation Date: {observation_date}')
+        # Add a small blurb about the bird
+        st.write("Bird description or blurb goes here.")
+        # Trending status could be calculated based on historical data or other factors
+        st.write("Trending: Upward/Downward")
